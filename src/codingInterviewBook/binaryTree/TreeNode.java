ba26dd5a33f4ad0b1ca2d4a7def382ec94364977
ivyxjc <ivyxjc1994@hotmail.com>
2017-04-06 21:10:27 +0800

    清理gradle

---
+++ src/codingInterviewBook/binaryTree/TreeNode.java
@@ -0,0 +1,16 @@
+package codingInterviewBook.binaryTree;
+
+/**
+ * Created by jc on 3/11/2017.
+ */
+
+
+public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
